219615346b0bf02cb88fd1cf72befd8f95f51230
TavoTevasC <212029088+TavoTevasC@users.noreply.github.com>
2026-04-02 18:43:44 +0300

    Update

--- src/app/components/category/category.ts
+++ src/app/components/category/category.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { CategoriesService, Category as CategoryModel } from '../../services/categories.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './category.html',
   styleUrl: './category.css',
 })
-export class Category {}
+export class Category implements OnInit {
+  categories = signal<CategoryModel[]>([]);
+  private categoriesService = inject(CategoriesService);
+
+  ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe({
+      next: (data) => this.categories.set(data),
+      error: (err) => {
+        console.error('Failed to load categories', err);
+      },
+    });
+  }
+
+  getCategoryIcon(name: string) {
+    return this.categoriesService.getIcon(name);
+  }
+}
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -18,4 +18,16 @@ export class CategoriesService {
     getCategories(): Observable<Category[]> {
         return this.http.get<Category[]>(this.apiUrl);
     }
+    
+    categoryIcons: { [key: string]: string } = {
+    'Elektronika': 'assets/icons/icons8-electronics-94.png',
+    'Apranga': 'assets/icons/icons8-clothes-94.png',
+    'Raktai': 'assets/icons/icons8-bunch-of-keys-94.png',
+    'Dokumentai': 'assets/icons/icons8-card-wallet-94.png',
+    'Kita': 'assets/icons/icons8-open-box-94.png'
+  };
+
+    getIcon(name: string): string {
+    return this.categoryIcons[name] || 'assets/icons/icons8-open-box-94.png';
+  }
 }
